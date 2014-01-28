482fd9039858f75490b02df5d1f0f186674c383f
xmqywx <xmqywx@163.com>
2014-01-28 23:29:09 +0800

    ss

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 // development only
 if ('development' == app.get('env')) {
